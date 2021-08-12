cc3953d593ad7075c65319b80e299a9b2b792dba
josue <eversonmelgar4@gmail.com>
2021-08-12 10:12:23 -0600

    comits3

--- Devjosue/settings.py
+++ Devjosue/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x(xoeh7bb3@mz8wx#o3p3u*-wlk+f!ui!0wr^m)9i*upg=xbar'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['#']
 
