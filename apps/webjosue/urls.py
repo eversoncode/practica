79da98936f64231e46ec0f961bbbe5eb74df3dc9
josue <eversonmelgar4@gmail.com>
2021-09-07 09:05:10 -0600

    hola

--- apps/webjosue/urls.py
+++ apps/webjosue/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import goku
+from .views import *
 
 
 urlpatterns = {
     path('', goku, name='wenos'),
+    path('', contact, name= 'contact')
 }
--- apps/webjosue/views.py
+++ apps/webjosue/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def goku(request):
     return render(request, 'index.html')
+
+def contact(request):
+    return render(request,'contact.html')
