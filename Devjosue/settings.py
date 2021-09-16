b9a3cec85454a041f911bdf4e5b3529f4798e7c3
josue <eversonmelgar4@gmail.com>
2021-09-16 08:48:28 -0600

    cambios 2

--- Devjosue/settings.py
+++ Devjosue/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 
 }
 """
-"""
+
 import dj_database_url
 from decouple import config
 
@@ -98,7 +98,7 @@ DATABASES = {
     )
 
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
